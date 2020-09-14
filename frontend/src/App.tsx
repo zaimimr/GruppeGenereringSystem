7fe2a23cb4c0cbbd60be6b22ae48a5e49a519ec1
Max Torre Schau <max.torre.schau@gmail.com>
2020-09-14 12:08:13 +0200

    Added typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+export type AppProps = {};
+
+const App: React.FunctionComponent<AppProps> = () => {
+  return (
+    <div className='App'>
+      <h1>Gruppegenereringssystem v1</h1>
+    </div>
+  );
+};
+
+export default App;
